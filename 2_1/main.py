e4f0e4190778d18be9ae51cd45637ed4e81091f9
kirill35476 <tr.kirilllll@gmail.com>
2024-10-12 15:52:31 +0300

    hit

--- 2_1/main.py
+++ 2_1/main.py
@@ -2,8 +2,20 @@ from hitbox import Hitbox
 
 
 #1
-hb1 = Hitbox(10,10,100,100)
-print(hb1.x)
-print(hb1.y)
-print(hb1.width)
-print(hb1.height)
+hb1 = Hitbox(0,100,100,100)
+
+#2
+hb2 = Hitbox(150,100,100,100)
+
+print(f'верхняя граница hb1: {hb1.top},'
+      f'верхняя граница hb2: {hb2.top},')
+print(f'нижния граница hb1: {hb1.bottom},'
+      f'нижния граница hb2: {hb2.bottom},')
+print(f'левая граница hb1: {hb1.left},'
+      f'левая граница hb2: {hb2.left},')
+print(f'правая граница hb1: {hb1.right},'
+      f'правая граница hb2: {hb2.right},')
+
+intersects = hb1.intersects(hb2)
+
+print(intersects)
